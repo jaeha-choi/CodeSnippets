0cf766bc6f236db282b2e74a1215908ef940d710
Jaeha Choi <jaeha220@gmail.com>
2020-11-16 06:03:32 -0800

    Add level_4.py

--- codesignal/level_4.py
+++ codesignal/level_4.py
@@ -17,3 +17,22 @@ def addBorder(picture):
         ret.append("*" + elem + "*")
     ret.append(top)
     return ret
+
+
+def areSimilar(a, b):
+    if a == b:
+        return True
+    elif sorted(a) != sorted(b):
+        return False
+    else:
+        temp1 = 0
+        count = 0
+        for elem1, elem2 in zip(a, b):
+            if elem1 != elem2 and count == 0:
+                temp1 = elem1
+                print(temp1)
+                count += 1
+            elif elem1 != elem2 and count == 1:
+                if temp1 != elem2:
+                    return False
+    return True
