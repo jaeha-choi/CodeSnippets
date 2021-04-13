c07237a1e2b5f3b971d1f6c3cc009074bd72f39f
Jaeha Choi <jaeha220@gmail.com>
2021-04-13 12:18:19 -0700

    Add contains-duplicate.py

---
+++ leetcode/contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+    # Inplace
+    # def containsDuplicate(self, nums: List[int]) -> bool:
+    #     nums.sort()
+    #     temp = nums[0]
+    #     for x in nums[1:]:
+    #         if temp == x:
+    #             return True
+    #         temp = x
+    #     return False
