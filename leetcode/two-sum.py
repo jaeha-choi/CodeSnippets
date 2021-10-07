029d057138313ecddbe801178f4810165b0f0be8
Jaeha Choi <jaeha@mail.jaeha.dev>
2021-10-07 11:25:33 -0700

    Add second answer for two-sum.py

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,8 +1,21 @@
+# Old answer
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for x, elem in enumerate(nums):
+#             temp = target - elem
+#             if temp in nums[:x]:
+#                 return [x, nums[:x].index(temp)]
+#             elif temp in nums[x + 1:]:
+#                 return [x, nums[x + 1:].index(temp) + 1 + x]
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x, elem in enumerate(nums):
-            temp = target - elem
-            if temp in nums[:x]:
-                return [x, nums[:x].index(temp)]
-            elif temp in nums[x + 1:]:
-                return [x, nums[x + 1:].index(temp) + 1 + x]
+        s = {}
+        for i, elem in enumerate(nums):
+            if (target - elem) in s:
+                return [s[target - elem][0], i]
+            else:
+                s.setdefault(elem, [])
+                s[elem].append(i)
