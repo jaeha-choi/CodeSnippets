1c0ba04f873791232619d4b91580556611a934d2
Jaeha Choi <jaeha@mail.jaeha.dev>
2021-11-12 13:50:55 -0800

    Add another attempt for 104

--- leetcode/100-199/104.maximum-depth-of-binary-tree.py
+++ leetcode/100-199/104.maximum-depth-of-binary-tree.py
@@ -10,13 +10,27 @@ class Solution:
     # def maxDepth(self, root: TreeNode, size) -> int:
     #     mx = 0
     #     if
+    # def maxDepth(self, root: TreeNode) -> int:
+    #     if root is None:
+    #         return 0
+    #     left = 0
+    #     if root.left is not None:
+    #         left += self.maxDepth(root.left)
+    #     right = 0
+    #     if root.right is not None:
+    #         right += self.maxDepth(root.right)
+    #     return max(left, right) + 1
+
+    # second attempt
     def maxDepth(self, root: TreeNode) -> int:
-        if root is None:
-            return 0
-        left = 0
-        if root.left is not None:
-            left += self.maxDepth(root.left)
-        right = 0
-        if root.right is not None:
-            right += self.maxDepth(root.right)
-        return max(left, right) + 1
+        def solve(node):
+            l = r = 1
+            if not node:
+                return 0
+            if node.left:
+                l += solve(node.left)
+            if node.right:
+                r += solve(node.right)
+            return max(l, r)
+
+        return solve(root)
